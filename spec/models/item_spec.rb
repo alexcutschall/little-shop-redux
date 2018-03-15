584cc882be7fd6ca6794e019aab4b6c27ae77334
VadLusk <AdLusk@gmail.com>
2018-03-15 14:32:22 -0600

    Fix validations for item.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,22 +1,22 @@
 RSpec.describe Item do
   describe 'Validate fields' do
     it 'is invalid to not have a title' do
-      item = Item.new(description: '', price: 12, image: '')
+      item = Item.new(description: 'jhg', price: 12, image: 'jhkjh')
 
       expect(item).to be_invalid
     end
     it 'is invalid to not have a description' do
-      item = Item.new(title: '', price: 12, image: '')
+      item = Item.new(title: ',jh', price: 12, image: 'jlhkjh')
 
       expect(item).to be_invalid
     end
     it 'is invalid to not have a price' do
-      item = Item.new(title: '', description: '', image: '')
+      item = Item.new(title: 'khkjh', description: 'jhjg', image: 'mmnb')
 
       expect(item).to be_invalid
     end
     it 'is invalid to not have an image' do
-      item = Item.new(title: '', description: '', price: 12)
+      item = Item.new(title: 'jgf', description: 'g', price: 12)
 
       expect(item).to be_invalid
     end
