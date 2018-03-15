20a121efec0917225d0c874d441000cb51f3547e
VadLusk <AdLusk@gmail.com>
2018-03-15 13:56:40 -0600

    Add create new item button.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -3,22 +3,22 @@ RSpec.describe Item do
     it 'is invalid to not have a title' do
       item = Item.new(description: '', price: 12, image: '')
 
-      expect(item).to_not be_valid
+      expect(item).to be_invalid
     end
     it 'is invalid to not have a description' do
       item = Item.new(title: '', price: 12, image: '')
 
-      expect(item).to_not be_valid
+      expect(item).to be_invalid
     end
     it 'is invalid to not have a price' do
       item = Item.new(title: '', description: '', image: '')
 
-      expect(item).to_not be_valid
+      expect(item).to be_invalid
     end
     it 'is invalid to not have an image' do
       item = Item.new(title: '', description: '', price: 12)
 
-      expect(item).to_not be_valid
+      expect(item).to be_invalid
     end
   end
 end
