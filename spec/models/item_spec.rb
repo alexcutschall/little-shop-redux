2b491933aa4ebeddf8eb77058f4026247fbe3c76
VadLusk <AdLusk@gmail.com>
2018-03-15 08:57:01 -0600

    Add test for new item button.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -2,26 +2,38 @@ RSpec.describe Item do
   describe 'Validate fields' do
     it 'is invalid to not have a title' do
       item = Item.new(description: '', price: 12, image: '')
+
       expect(item).to_not be_valid
     end
     it 'is invalid to not have a description' do
       item = Item.new(title: '', price: 12, image: '')
+
       expect(item).to_not be_valid
     end
     it 'is invalid to not have a price' do
       item = Item.new(title: '', description: '', image: '')
+
       expect(item).to_not be_valid
     end
     it 'is invalid to not have an image' do
       item = Item.new(title: '', description: '', price: 12)
+
       expect(item).to_not be_valid
     end
   end
-  describe 'Page navigation', :type => :feature do
+  describe 'Page navigation', type: :feature do
     it 'shows all items' do
       visit '/items'
 
       expect(page).to have_content('Items')
     end
+    it 'takes you from index to new' do
+      visit '/items'
+      click_button 'Create a new item'
+
+      expect(page).has_xpath?('//items/new')
+    end
+
+
   end
 end
