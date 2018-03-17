6e156c86f9931791ab3b2559b866ec869eb72ead
VadLusk <AdLusk@gmail.com>
2018-03-16 23:47:07 -0600

    Refactor link to <a> instead of <form>.

--- spec/features/items/index_navigation_spec.rb
+++ spec/features/items/index_navigation_spec.rb
@@ -5,11 +5,21 @@ RSpec.describe Item, type: :feature do
 
       expect(page).to have_content('Items')
     end
+    it 'has a create new item link' do
+      visit '/items'
+
+      expect(page).to have_link('Create A New Item')
+    end
     it 'takes you from index to new' do
       visit '/items'
       click_button 'Create a new item'
 
-      expect(current_path).to have_xpath('/items/new')
+      expect(current_path).to eql('/items/new')
+    end
+    it 'has an item dashboard link' do
+      visit '/items'
+
+      expect(page).to have_link('Dashboard')
     end
   end
 end
