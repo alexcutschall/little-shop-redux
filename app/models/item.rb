c3a8e69da087e31949a8d19142048fb484181ce1
alexcutschall <alexander.cutschall@gmail.com>
2018-03-19 11:24:14 -0600

    trying some css

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,10 +15,4 @@ class Item < ActiveRecord::Base
     Item.sum("unit_price * quantity")
   end
 
-  def unit_price
-    price = invoice_items.reduce(0) do |total, invoice_item|
-      total + (invoice_item.unit_price / 100)
-    end
-    (price / 100).to_f
-  end
 end
