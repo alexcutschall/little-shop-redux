87a7cefa000b56c90a256fbb8c00f73cdeca17f5
VadLusk <AdLusk@gmail.com>
2018-03-18 18:03:21 -0600

    Fix tests, creating merchant everywhere merchant info displayed.

--- spec/features/items/navigation_spec.rb
+++ spec/features/items/navigation_spec.rb
@@ -24,7 +24,8 @@ RSpec.describe Item, type: :feature do
         expect(page).to have_link('x')
       end
       it 'takes user from index to show' do
-        Item.create(title: 'x', description: 'x', price: 12, image: 'x')
+        Item.create(title: 'x', description: 'x', price: 12, image: 'x', merchant_id: 1)
+        Merchant.create(name: 'x')
         visit '/items'
         click_link 'x'
 
@@ -33,33 +34,37 @@ RSpec.describe Item, type: :feature do
     end
     context 'Show page' do
       it 'has an edit link' do
-        Item.create(title: 'x', description: 'x', price: 12, image: 'x')
+        Item.create(title: 'x', description: 'x', price: 12, image: 'x', merchant_id: 1)
+        Merchant.create(name: 'x')
         visit 'items/1'
 
         expect(page).to have_link('Edit')
       end
       it 'takes user from show to edit' do
-        Item.create(title: 'x', description: 'x', price: 12, image: 'x')
+        Item.create(title: 'x', description: 'x', price: 12, image: 'x', merchant_id: 1)
+        Merchant.create(name: 'x')
         visit 'items/1'
         click_link 'Edit'
 
         expect(current_path).to eql('/items/1/edit')
       end
       it 'has a delete link' do
-        Item.create(title: 'x', description: 'x', price: 12, image: 'x')
+        Item.create(title: 'x', description: 'x', price: 12, image: 'x', merchant_id: 1)
+        Merchant.create(name: 'x')
         visit 'items/1'
 
         expect(page).to have_link('Delete')
       end
       it 'takes user from show to index after deleting' do
-        Item.create(title: 'x', description: 'x', price: 12, image: 'x')
+        Item.create(title: 'x', description: 'x', price: 12, image: 'x', merchant_id: 1)
+        Merchant.create(name: 'x')
         visit 'items/1'
         click_link 'Delete'
 
         expect(current_path).to eql('/items')
       end
       it 'takes user to the item\'s merchant page' do
-        Item.create(title: 'Thing', description: 'does stuff', price: 12, image: 'URL')
+        Item.create(title: 'Thing', description: 'does stuff', price: 12, image: 'URL', merchant_id: 1)
         Merchant.create(name: 'Merch name')
         visit '/items/1'
         click_link 'Merch name'
