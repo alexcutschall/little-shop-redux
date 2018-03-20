fcbaad03d56f90d2053f1179289eee0316c7f193
VadLusk <AdLusk@gmail.com>
2018-03-20 17:42:50 -0600

    Item content spec passes. Add first merchant model class method test.

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,10 +1,25 @@
 RSpec.describe Merchant do
-
-  describe 'Validate fields' do
+  context 'Validate fields' do
     it 'is invalid to not have a name' do
       merchant = Merchant.new(id: 12334105)
 
       expect(merchant).to_not be_valid
+    end
+  end
+  context 'Class methods' do
+    it '.most_items' do
+      Merchant.create(name: 'First')
+      Item.create(title: 'Thing', description: 'x', price: 12, image: 'x', merchant_id: 1)
+      Item.create(title: 'Another thing', description: 'x', price: 12, image: 'x', merchant_id: 1)
+      Item.create(title: 'Something else', description: 'x', price: 12, image: 'x', merchant_id: 1)
+      Item.create(title: 'Anything', description: 'x', price: 12, image: 'x', merchant_id: 1)
+      Item.create(title: 'Stuff', description: 'x', price: 12, image: 'x', merchant_id: 1)
+      Merchant.create(name: 'First')
+      Item.create(title: 'Thing', description: 'x', price: 12, image: 'x', merchant_id: 1)
+      Merchant.create(name: 'First')
+      Item.create(title: 'Thing', description: 'x', price: 12, image: 'x', merchant_id: 1)
+
+      expect(Merchant.most_items).to eql('adf')
     end
   end
 end
