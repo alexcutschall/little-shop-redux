3267742acbaeadbdbe4bbd0183ce30671f57c6de
VadLusk <AdLusk@gmail.com>
2018-03-20 22:01:21 -0600

    Finish class method tests for invoice item model.

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -23,17 +23,49 @@ RSpec.describe 'InvoiceItem model' do
   end
   context 'Class methods' do
     it '.total_price' do
-      InvoiceItem.create(item_id: 2, invoice_id: 5 , quantity: 6, unit_price: 1000)
+      InvoiceItem.create(item_id: 2, invoice_id: 5, quantity: 6, unit_price: 1000)
       InvoiceItem.create(item_id: 2, invoice_id: 9, quantity: 6, unit_price: 1000)
       InvoiceItem.create(item_id: 1, invoice_id: 5, quantity: 6, unit_price: 1000)
       InvoiceItem.create(item_id: 2, invoice_id: 90, quantity: 6, unit_price: 1000)
 
       expect(InvoiceItem.total_price).to eq(240.0)
     end
+    it '.highest_unit_price' do
+      InvoiceItem.create(item_id: 2, invoice_id: 5, quantity: 6, unit_price: 1000)
+      InvoiceItem.create(item_id: 2, invoice_id: 9, quantity: 6, unit_price: 150)
+      InvoiceItem.create(item_id: 1, invoice_id: 54, quantity: 6, unit_price: 5000)
+      InvoiceItem.create(item_id: 2, invoice_id: 90, quantity: 6, unit_price: 100)
+
+      expect(InvoiceItem.highest_unit_price).to eq(54)
+    end
+    it '.lowest_unit_price' do
+      InvoiceItem.create(item_id: 2, invoice_id: 5, quantity: 6, unit_price: 1000)
+      InvoiceItem.create(item_id: 2, invoice_id: 9, quantity: 6, unit_price: 150)
+      InvoiceItem.create(item_id: 1, invoice_id: 54, quantity: 6, unit_price: 5000)
+      InvoiceItem.create(item_id: 2, invoice_id: 90, quantity: 6, unit_price: 100)
+
+      expect(InvoiceItem.lowest_unit_price).to eq(90)
+    end
+    it '.largest_quantity' do
+      InvoiceItem.create(item_id: 2, invoice_id: 5, quantity: 64, unit_price: 1000)
+      InvoiceItem.create(item_id: 2, invoice_id: 9, quantity: 6, unit_price: 150)
+      InvoiceItem.create(item_id: 1, invoice_id: 54, quantity: 20, unit_price: 5000)
+      InvoiceItem.create(item_id: 2, invoice_id: 90, quantity: 9, unit_price: 100)
+
+      expect(InvoiceItem.largest_quantity).to eq(5)
+    end
+    it '.smallest_quantity' do
+      InvoiceItem.create(item_id: 2, invoice_id: 5, quantity: 64, unit_price: 1000)
+      InvoiceItem.create(item_id: 2, invoice_id: 9, quantity: 6, unit_price: 150)
+      InvoiceItem.create(item_id: 1, invoice_id: 54, quantity: 20, unit_price: 5000)
+      InvoiceItem.create(item_id: 2, invoice_id: 90, quantity: 9, unit_price: 100)
+
+      expect(InvoiceItem.smallest_quantity).to eq(9)
+    end
   end
   context 'Instance methods' do
     it 'find_title' do
-      inv_item = InvoiceItem.create(item_id: 1, invoice_id: 5 , quantity: 6, unit_price: 10)
+      inv_item = InvoiceItem.create(item_id: 1, invoice_id: 5, quantity: 6, unit_price: 10)
       Item.create(title: 'Thing', description: 'x', price: 6, image: 'x', merchant_id: 9)
 
       expect(inv_item.find_title(1)).to eq('Thing')
