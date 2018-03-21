d622eaec18b0e0f8588ed0d5c7013edb41f4c557
VadLusk <AdLusk@gmail.com>
2018-03-20 23:52:14 -0600

    Clean up invoice nav and content tests, delete a few duplicates.

--- spec/features/invoices/content_spec.rb
+++ spec/features/invoices/content_spec.rb
@@ -1,5 +1,5 @@
-describe Invoice, type: :feature do
-  context 'Page navigation' do
+describe Invoice do
+  context 'Page content' do
     context 'Index page' do
       it 'shows the word invoices' do
         visit '/invoices'
@@ -16,19 +16,8 @@ describe Invoice, type: :feature do
         expect(page).to have_content('delivered')
         expect(page).to have_content('shipped')
       end
-      it 'contains invoice links by id' do
-        Invoice.create(merchant_id: 1, status: 'pending')
-        Invoice.create(merchant_id: 30, status: 'delivered')
-        Invoice.create(merchant_id: 35, status: 'shipped')
-        visit '/invoices'
-
-        expect(page).to have_text('pending')
-        expect(page).to have_link('2', href: '/invoices/2')
-        expect(page).to have_link('3', href: '/invoices/3')
-      end
       it 'should redirect to an invoice page when you click the link' do
         Invoice.create(merchant_id: 1, status: 'pending')
-
         visit '/invoices'
         click_link '1'
 
@@ -36,28 +25,31 @@ describe Invoice, type: :feature do
       end
     end
     context 'Show page' do
-      it 'should have invoice information' do
-        Invoice.create(merchant_id: 30, status: 'pending')
-        Invoice.create(merchant_id: 1, status: 'delivered')
-        Merchant.create(name: 'First')
+      it 'should have that invoice information' do
+        inv = Invoice.create(merchant_id: 45, status: 'pending')
+        visit '/invoices/1'
 
-        visit '/invoices/2'
-
-        expect(page).to have_text('Invoice ID: 2')
-        expect(page).to have_text('Merchant ID: 1')
-        expect(page).to have_text('Status: delivered')
+        expect(page).to have_text('1')
+        expect(page).to have_text('45')
+        expect(page).to have_text('pending')
       end
+
       it 'should have edit and delete buttons' do
         Invoice.create(merchant_id: 1, status: 'pending')
         Invoice.create(merchant_id: 30, status: 'delivered')
+
         visit '/invoices/2'
 
-        page.has_button?('Edit')
-        page.has_button?('Delete')
+        page.has_button?('edit')
+        page.has_button?('delete')
       end
+
       it 'should take you to an edit page when you click edit button' do
+
       end
+
       it 'should take you to a page to confirm that you want to delete the item' do
+
       end
     end
   end
--- spec/features/merchants/user_can_use_navigation_bar_spec.rb
+++ spec/features/merchants/user_can_use_navigation_bar_spec.rb
@@ -1,23 +1,23 @@
 RSpec.describe Merchant do
-  describe "user is on the page merchant/items/invoices" do
-    context "user can navigate around the website" do
-      it "takes user to merchant page" do
-        visit '/merchants'
+  describe 'user is on the page merchant/items/invoices' do
+    context 'user can navigate around the website' do
+      it 'takes user to merchant page' do
+        visit '/'
         click_button 'Merchants'
 
-        expect(page).to have_content("Merchants")
+        expect(page).to have_content('Merchants')
       end
-      it "takes user to invoices page" do
+      it 'takes user to invoices page' do
         visit '/merchants'
         click_button 'Invoices'
 
-        expect(page).to have_content("Invoices")
+        expect(page).to have_content('Invoices')
       end
-      it "takes user to items page" do
+      it 'takes user to items page' do
         visit '/merchants'
         click_button 'Items'
 
-        expect(page).to have_content("Items")
+        expect(page).to have_content('Items')
       end
     end
   end
